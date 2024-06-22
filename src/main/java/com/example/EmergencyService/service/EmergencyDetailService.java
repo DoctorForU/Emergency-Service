7380c66ea9fe2a14f2279f7aa58278a303068570
GITYHS <154618398+GITYHS@users.noreply.github.com>
2024-06-23 02:33:23 +0900

    feat. Add service

--- src/main/java/com/example/EmergencyService/service/EmergencyDetailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.EmergencyService.service;
-
-public class EmergencyDetailService { //응급의료기관 기본정보 조회 -> 5
-
-    //HPID필요
-}
--- src/main/java/com/example/EmergencyService/service/EmergencyListService.java
+++
@@ -1,9 +0,0 @@
-package com.example.EmergencyService.service;
-
-public class EmergencyListService { // 응급의료기관 목록정보 조회 -> 3
-    // 주소 Q0 -> 시도
-    // Q1 -> 시군구
-    // QT -> 진료요일 -> 월~일: 1~7, 공휴일: 8
-
-    // Response에 hpid필요함
-}
